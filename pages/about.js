780ebde48c82a4167b0c735742ca3451feefb917
Osi4lucy <osi4lucy@gmail.com>
2022-01-25 13:39:50 +0100

    about and contact css created

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
+import styles from '../styles/About.module.css';
+
 const About = () => {
 	return (
-		<div>
+		<div className={styles.about}>
 			<h1 className='bg-white text-center font-semibold'>About Page</h1>
 		</div>
 	);
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
+import styles from '../styles/Contact.module.css';
 const Contact = () => {
-	return <h1 className='bg-white text-center font-semibold'>Contact Us</h1>;
+	return <h1 className={styles.contact}>Contact Us</h1>;
 };
 
 export default Contact;
