b2b305fad12d227daa8ac1661eb5b9d074d66f00
Osi4lucy <osi4lucy@gmail.com>
2022-01-26 13:56:08 +0100

    footer styles added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,40 @@
 import styles from '../styles/Footer.module.css';
+import Link from 'next/link';
+import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
 		<div className={styles.footer}>
-			<h3> Osilamah-Solomon-Imhomoh 2022</h3>
+			<h2> Follow Us</h2>
+			<ul className={styles.social_icons}>
+				<Link href='/'>
+					<a>
+						<FaFacebook />
+					</a>
+				</Link>
+				<Link href='/'>
+					<a>
+						<FaTwitter />
+					</a>
+				</Link>
+				<Link href='/'>
+					<a>
+						<FaInstagram />
+					</a>
+				</Link>
+			</ul>
+			<ul className={styles.menu}>
+				<Link href='/'>
+					<a>Home</a>
+				</Link>
+				<Link href='/'>
+					<a>Submit a Recipe</a>
+				</Link>
+				<Link href='/'>
+					<a>Contact Us</a>
+				</Link>
+			</ul>
+			<p>Food Blog App | 2022</p>
 		</div>
 	);
 };
